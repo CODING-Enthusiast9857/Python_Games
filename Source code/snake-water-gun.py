1f5bb0d6a82b069a0d5589bcdcc1b8ee6b068631
Madhavi Sonawane <108464846+CODING-Enthusiast9857@users.noreply.github.com>
2022-11-10 18:38:19 +0530

    Updated

--- Source code/snake-water-gun.py
+++ Source code/snake-water-gun.py
@@ -1,1 +1,25 @@
-
+import random
+print("Snake water gun")
+options=["s","w","g"]				
+print("\nSnake - s\nWater - w\nGun - g\n")
+userch=input("Enter your choice : ")
+if userch in options:
+	compch=random.choice(options)
+	if compch=="s":
+		if userch=="w":
+			print("Computer won")
+		else:
+			print("You won")
+	elif compch=="w":
+		if userch=="g":
+			print("Computer won")
+		else:
+			print("You won") 
+	else:
+		if userch=="s":
+			print("Computer won")
+		else:
+			print("You won")	
+else:
+	print("Select correct option next time")
+	exit()
