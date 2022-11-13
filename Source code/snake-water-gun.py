dacfec7dde6d33835ac4802a2900277ab2c4462c
Madhavi Sonawane <madhavison06@gmail.com>
2022-11-13 14:24:42 +0530

    Snake game updated

--- Source code/snake-water-gun.py
+++ Source code/snake-water-gun.py
@@ -2,24 +2,32 @@ import random
 print("Snake water gun")
 options=["s","w","g"]				
 print("\nSnake - s\nWater - w\nGun - g\n")
-userch=input("Enter your choice : ")
-if userch in options:
+
+while True:
+	userch=input("\nEnter your choice : ")
 	compch=random.choice(options)
-	if compch=="s":
-		if userch=="w":
-			print("Computer won")
+	print("\nComputer choose ",compch,", User choose ",userch,"\n")
+	
+	if userch==compch:
+		print(f"Both choose {compch}, It's Tie!!")
+	elif userch=='s':
+		if compch=='w':
+			print("Snake drinks water! You won.")
 		else:
-			print("You won")
-	elif compch=="w":
-		if userch=="g":
-			print("Computer won")
+			print("Gun kills snake! You lose.")
+	elif userch=='w':
+		if compch=='g':
+			print("Gun drowned water! You won.")
 		else:
-			print("You won") 
-	else:
-		if userch=="s":
-			print("Computer won")
+			print("Snake drinks water! You lose.")
+	elif userch=='g':
+		if compch=='s':
+			print("Gun kills snake! You won.")
 		else:
-			print("You won")	
-else:
-	print("Select correct option next time")
-	exit()
+			print("Gun drowned water! You lose.")
+	elif userch not in options:
+		print("Incorrect Choice! Please Choose from given options next time.")
+
+	con=input("Play Again(y/n) --> ")
+	if con.lower()!='y':
+		break
