b655c94bc88dc2c61728c338d93cb252a7d12ebd
Madhavi Sonawane <108464846+CODING-Enthusiast9857@users.noreply.github.com>
2022-11-16 15:40:44 +0530

    FLAMES game added

--- Source code/FLAMES_game.py
+++ Source code/FLAMES_game.py
@@ -1,1 +1,26 @@
-
+def flames(n1,n2):
+	con=n1+n2
+	
+	for i in con:
+		if con.count(i)!=1:
+			con=con.replace(i,'')
+		
+	n=len(con)%6
+	
+	if n==1:
+	   return "Friends"
+	elif n==2:
+	   return "Love"
+	elif n==3:
+	   return "Affection"
+	elif n==4:
+	   return "Marriage"
+	elif n==5:
+	   return "Enemy"
+	elif n==6:
+	   return "Siblings"
+    	
+n1=input("Enter 1st name : ")
+n2=input("Enter 2nd name : ")
+print("\nFLAMES : \nF - Friends\nL - Love\nA - Affection\nM - Marriage\nE - Enemy\nS - Siblings\n")
+print("Relationship status is :",flames(n1,n2))
